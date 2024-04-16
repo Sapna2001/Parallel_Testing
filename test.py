876ba5709ef2a8f53b2cfba9b7b87190c96a076d
Sapna2001 <harisapnanair@gmail.com>
2024-04-16 18:42:47 +0530

    fail msg added

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from parallel_testing import get_chrome_title, get_edge_title
  
 def test_get_chrome_title():
     title = get_chrome_title()
-    assert title == "Your Store"
+    assert title == "Your Store", "Test not passed for chrome"
  
 def test_get_edge_title():
     title = get_edge_title()
-    assert title == "Your Store"
+    assert title == "Your Store", "Test not passed for edge"
